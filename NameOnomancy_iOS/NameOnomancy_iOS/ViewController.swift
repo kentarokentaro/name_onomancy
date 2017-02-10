350f32c8266d2063a33cb5b1b5f924c3299a1058
kentarokentaro <oikimutida@yahoo.co.jp>
2017-02-10 11:55:55 +0900

    add function when textField is empty

--- NameOnomancy_iOS/NameOnomancy_iOS/ViewController.swift
+++ NameOnomancy_iOS/NameOnomancy_iOS/ViewController.swift
@@ -23,10 +23,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // 
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "showResult" {
+            // テキストフィールドの文字が空の場合
+            if textField.text == "" {
+                return false
+            }
+            else {
+                return true
+            }
+        }
+        return true
+    }
+    
     // segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let resultViewController: ResultViewController = segue.destination as! ResultViewController
-        resultViewController.userName = textField.text!
+
+        // idnifier - sendボタンの場合
+        if segue.identifier == "showResult" {
+            let resultViewController: ResultViewController = segue.destination as! ResultViewController
+            // テキストフィールドの文字列を渡す
+            resultViewController.userName = textField.text!
+        }
     }
 
 
